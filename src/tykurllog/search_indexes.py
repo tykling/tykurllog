78b7e9e413f15fb0565bcd7807bb2421a29c272e
Thomas Steen Rasmussen <thomas@gibfest.dk>
2015-11-24 23:47:18 +0100

    switch to ngramfields for url and usermask

--- src/tykurllog/search_indexes.py
+++ src/tykurllog/search_indexes.py
@@ -3,9 +3,9 @@ from .models import LoggedUrl
 
 
 class LoggedUrlIndex(indexes.SearchIndex, indexes.Indexable):
-    text = indexes.CharField(model_attr='url', document=True)
+    text = indexes.NgramField(model_attr='url', document=True)
     when = indexes.DateTimeField(model_attr='when')
-    usermask = indexes.CharField(model_attr='usermask')
+    usermask = indexes.NgramField(model_attr='usermask')
     channel = indexes.CharField(model_attr='channel')
 
     def get_model(self):
